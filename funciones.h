14360901bec2733f65a77ab3e7ac2af16ef4726e
Jonathan Granda <jhonatangranda.2002@gmail.com>
2025-04-20 22:10:20 -0500

    Depuraciones funciones.h

--- funciones.h
+++ funciones.h
@@ -2,7 +2,7 @@
 #define FUNCIONES_H
 
 // Función que abre el directorio de los procesos donde buscaremos el/los procesos que listaremos
-int openDirectory(const char *ruta, const char *id, const char *command, char *infoMem);
+int openDirectory(const char *ruta, const char *id, char *infoMem);
 // Función para comparar un string es un número
 int esNumero(struct dirent *entry);
 // Función que lista o realiza un reporte según sea la entrada del comando
